ba67619e1d1d40d2f35e1f4441df289552300df3
Sergei Ikkert <sergei.ikkert@wago.com>
2022-02-08 13:31:08 +0100

    Implemented stopping of modbus server.

--- build-context/resources/home/kbus_modbus_server/start.sh
+++ build-context/resources/home/kbus_modbus_server/start.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
+DAEMON_NAME="kbusmodbusslave"
 
-cleanup() {
-  echo "stopping kbus-modbus-server daemon"
-  kbusmodbusslave stop
-  sleep 3 # todo detect daemon is stopped
+wait_for_stop()
+{
+  local PID=$1
+  local TIMEOUT=$2
+  local RET_VAL=0
+  local  TIMEOUT_COUNTER=0
+  if [ -n "${PID}" ]
+  then
+    while [ -d "/proc/${PID}" ]
+    do
+      if (( TIMEOUT_COUNTER > TIMEOUT ))
+      then
+        RET_VAL=1
+        break
+      else
+       TIMEOUT_COUNTER=$((TIMEOUT_COUNTER + 1))
+       sleep 1
+      fi
+    done
+  fi
 
-  echo "stopping all init daemons"
-  run-parts -a stop /etc/rc_kbus_modbus_server.d/ 
+  return $RET_VAL
 }
 
-run() {
+cleanup()
+{
+  DAEMONIZE="/sbin/start-stop-daemon"
+  echo "stopping all init daemons"
+  run-parts -a stop /etc/rc_kbus_modbus_server.d/ 
+
+  local PID=$(pidof $DAEMON_NAME)
+  echo -e "pidof $DAEMON_NAME:  ${PID}"  
+  "$DAEMONIZE" -K -qx "$DAEMON_NAME"
+  wait_for_stop ${PID} 5
+  if [ $? -ne 0 ]
+  then
+    echo -e "Regular stopping of $DAEMON_NAME failed send SIGKILL"    
+    killall -9 $DAEMON_NAME &> /dev/null
+  fi
+  echo "done"
+}
+
+run() 
+{
   echo "start init daemons"
   run-parts -a start /etc/rc_kbus_modbus_server.d/
 
-  echo "start kbus-modbus-server damon"
-  kbusmodbusslave -v 7 --nodaemon
+  echo -e "start $DAEMON_NAME"
+  kbusmodbusslave -v 7 --nodaemon &
   wait $!
 }
 
 trap cleanup INT TERM
 run
-
-
